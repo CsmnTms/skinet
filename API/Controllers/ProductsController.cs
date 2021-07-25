77da4b7338676bbc16240a4a69a2455c9059af88
Tamas Cosmin <csmntamas97@gmail.com>
2021-07-25 11:13:01 +0300

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController] // attribute to state that it is a controller
+    [Route("api/[controller]")] // routing
+    public class ProductsController : ControllerBase // because it is a controller, we need to derive from ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products); 
+        }
+
+        [HttpGet("{id}")] // the {id} connects to the method arg below to help the API differentiate between the methods by URL
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,57 @@
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace API
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config; // underscore means it's a private field
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container. 
+        // Referred to as the Dependency Injection Container. 
+        // Any services we want to add to our app that we then want to make available to other parts of our app, we add to this method.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            // gets the connection from the appsetings.dev.json
+            services.AddDbContext<StoreContext>(x => 
+                x.UseSqlite(_config.GetConnectionString("DefaultConnection")) 
+            ); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This is where we add middleware (which can do various things to requests)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection(); // if a request comes on the http url, it redirects it to the https
+
+            app.UseRouting(); // responsible for getting us to the controller that we're hitting
+
+            app.UseAuthorization();
+
+            // maps the endpoints in the controller so the API know where to send the requests to us
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
